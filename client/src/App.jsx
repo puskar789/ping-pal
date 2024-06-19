7d0303da84353334e770a6acb8c6391c6827ba52
puskar789 <pbohara231159@gmail.com>
2024-06-19 19:18:25 +0530

    doing UI design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable no-unused-vars */
+import Home from "./pages/home/Home.jsx";
 import Login from "./pages/login/Login.jsx";
+import SignUp from "./pages/signup/SignUp.jsx";
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Login />
+      <Home />
     </div>
   );
 }
