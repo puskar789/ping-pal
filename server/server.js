9b81bcd28721b08478da0f2d75c6946d0ed7ff76
puskar789 <pbohara231159@gmail.com>
2024-06-26 14:32:22 +0530

    implemented online status using socket.io

--- server/server.js
+++ server/server.js
@@ -8,9 +8,10 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
+import { app, server } from "./socket/socket.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
-const app = express();
+// const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -27,7 +28,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server is running on port ${PORT}`);
 });
