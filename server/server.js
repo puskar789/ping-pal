4896e41eec82ebc09be4e75a3132ec950a556373
puskar789 <pbohara231159@gmail.com>
2024-07-01 15:30:56 +0530

    added deployment code

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 // files
 import authRoutes from "./routes/auth.routes.js";
@@ -13,6 +14,8 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 
 // const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -27,6 +30,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
