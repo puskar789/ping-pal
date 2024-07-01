688cf9295d65e24585ecfeec7a102beb1e1ba5af
puskar789 <pbohara231159@gmail.com>
2024-07-01 16:16:45 +0530

    updated io path

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://ping-pal.onrender.com", {
         query: { userId: authUser._id },
       });
       setSocket(socket);
